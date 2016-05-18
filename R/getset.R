14c623492f3c9c6190986b7b2a959e53aaa9fc3f
Jean-daniel Zucker <jdzucker@gmail.com>
2016-05-18 19:48:54 +0700

    Make getmodelparameter silent.

--- R/getset.R
+++ R/getset.R
@@ -136,7 +136,7 @@ getmodelparameter <- function(modelfile,experimentname) {
                getOption("gamar.Xmx"),
                " -Djava.awt.headless=true org.eclipse.core.launcher.Main ",
                "-application msi.gama.headless.id4 -xml ",
-               experimentname," ",modelfile," ",outfile))
+               experimentname," ",modelfile," ",outfile,">/dev/null"))
 
   trycommand <- system(paste0("java -jar ",getOption("gamar.startjar")," -Xms",
                               getOption("gamar.Xms")," -Xmx",
