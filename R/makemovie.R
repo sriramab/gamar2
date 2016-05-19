ed1f39cd44c00d91e7657de5e8b8d2d522c7c993
Marc Choisy <mchoisy@gmail.com>
2016-05-19 16:30:56 +0700

    Changing the default output path in makemovie

--- R/makemovie.R
+++ R/makemovie.R
@@ -1,9 +1,10 @@
-makemovie <- function(experiment,output="animation.gif",...) {
+makemovie <- function(experiment,output,...) {
   mydir <- paste0(dirname(attr(experiment,"path")),"/snapshot/")
+  if(mising(output)) output <- paste0(mydir,"animation.gif")
   info_display <- experiment$info_display
   my_files <- unlist(lapply(info_display,function(x)paste0(mydir,x)))
   animation::ani.options(interval=.01,loop=T,autobrowse=F,autoplay=T,
                        nmax=length(info_display))
   animation::ani.options(...)
-  animation::im.convert(files=my_files,output=paste0(getwd(),"/",output))
+  animation::im.convert(files=my_files,output=output)
 }
